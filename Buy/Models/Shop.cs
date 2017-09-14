73c71715afeca0959bb58cd441a0004abc56cb98
sonicshadow <sonic_458@126.com>
2017-09-14 12:01:52 +0800

    添加线下券

---
+++ Buy/Models/Shop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Buy.Models
+{
+    public class Shop
+    {
+        public virtual int ID { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string Logo { get; set; }
+
+        public virtual int Sort { get; set; }
+
+        public virtual string Code { get; set; }
+
+
+        public virtual List<LocalCoupon> Coupons { get; set; }
+
+        public virtual List<ShopMember> Members { get; set; }
+    }
+}
