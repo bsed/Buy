121e4ce8bacab58c8f5542a2519c20599aff9627
sonicshadow <sonic_458@126.com>
2017-09-20 01:12:02 +0800

    新的蘑菇街分类

--- Buy/Controllers/TestController.cs
+++ Buy/Controllers/TestController.cs
@@ -13,12 +13,28 @@ namespace Buy.Controllers
 {
     public class TestController : Controller
     {
-        // GET: Test
+        [AllowCrossSiteJson]
         public ActionResult Index(string phone)
         {
-            ISms sms = new YunPianSms();
-            sms.Send(phone, Comm.Random.Next(1000, 9999).ToString());
-            return Json("1");
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                //var c = db.CouponTypes
+                //   .Where(s => s.Platform == Enums.CouponPlatform.MoGuJie && s.Keyword != null)
+                //   .ToList();
+                //foreach (var item in c)
+                //{
+                //    item.Keyword = item.Keyword.Replace(";", ",");
+                //}
+                //db.SaveChanges();
+                var cid = db.CouponTypes
+                    .Where(s => s.Platform == Enums.CouponPlatform.MoGuJie && s.Keyword != null)
+                    .ToList()
+                    .Select(s => s.Keyword.SplitToArray<string>(','))
+                    .SelectMany(s => s)
+                    .ToList();
+                return Json(Comm.ToJsonResult("Success", "", cid), JsonRequestBehavior.AllowGet);
+            }
+
         }
     }
 
