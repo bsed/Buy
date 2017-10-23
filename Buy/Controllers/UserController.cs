42645c2d6a503e5fbaae54c106881a5d7e154955
sonicshadow <sonic_458@126.com>
2017-10-23 13:37:07 +0800

    修复修改用户信息修改微信号无效

--- Buy/Controllers/UserController.cs
+++ Buy/Controllers/UserController.cs
@@ -48,7 +48,7 @@ namespace Buy.Controllers
         [AllowCrossSiteJson]
         [AllowAnonymous]
         [HttpPost]
-        public ActionResult Edit(string userID, string nickName, string avatar, string wecharCode)
+        public ActionResult Edit(string userID, string nickName, string avatar, string wechatCode)
         {
             var user = db.Users.FirstOrDefault(s => s.Id == userID);
             if (user == null)
@@ -57,7 +57,7 @@ namespace Buy.Controllers
             }
             user.NickName = nickName;
             user.Avatar = avatar;
-            user.WeChatCode = wecharCode;
+            user.WeChatCode = wechatCode;
             db.SaveChanges();
             return Json(Comm.ToJsonResult("Success", "修改成功"));
         }
