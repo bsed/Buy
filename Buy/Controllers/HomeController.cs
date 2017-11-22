90e104a887b53178121b53a6956ca133a67cf66d
LiangHuiWen123 <blueameng@163.com>
2017-11-22 11:27:29 +0800

    修改首頁二維碼

--- Buy/Controllers/HomeController.cs
+++ Buy/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Buy.Models;
+
 namespace Buy.Controllers
 {
     public class HomeController : Controller
@@ -19,9 +20,31 @@ namespace Buy.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+
+            if (Comm.IsWeChat)
+                ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        public ActionResult Download()
+        {
+            //string sda = "~/Download/malieme.apk";
+            if (Comm.IsMobileDrive)
+            {
+                string agent = System.Web.HttpContext.Current.Request.UserAgent.ToLower();
+                var type = agent.Contains("iphone") ? Enums.UpdateLogType.IOS : Enums.UpdateLogType.Android;
+                if (type == Enums.UpdateLogType.Android)
+                {
+                    string filePath = Server.MapPath("~/Download/malieme.apk");//路径
+                    return File(filePath, "text/plain", "malieme.apk"); //arrow-right.png是客户端保存的名字
+                }
+                else
+                {
+                    return Redirect("https://itunes.apple.com/cn/app/id1294184032?mt=8");
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         public ActionResult Contact()
