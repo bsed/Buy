6ee160f36992a03127e4b42185f4515046c34e91
sonicshadow <sonic_458@126.com>
2017-11-24 09:28:11 +0800

    Shop表添加TradingArea(商圈)字段

--- Buy/Models/Shop.cs
+++ Buy/Models/Shop.cs
@@ -28,23 +28,35 @@ namespace Buy.Models
 
         public virtual List<ShopMember> Members { get; set; }
 
+        [Display(Name = "简介")]
         public virtual string Remark { get; set; }
 
+        [Display(Name = "图片介绍")]
         public virtual string Images { get; set; }
 
+        [Display(Name = "联系电话")]
         public virtual string PhoneNumber { get; set; }
 
+        [Display(Name = "纬度")]
         public double? Lat { get; set; }
 
+        [Display(Name = "经度")]
         public double? Lng { get; set; }
 
+        [Display(Name = "详细地址")]
         public string Address { get; set; }
 
+        [Display(Name = "省份")]
         public string Province { get; set; }
 
+        [Display(Name = "城市")]
         public string City { get; set; }
 
+        [Display(Name = "地区")]
         public string District { get; set; }
+
+        [Display(Name = "商圈")]
+        public string TradingArea { get; set; }
     }
 
     [NotMapped]
